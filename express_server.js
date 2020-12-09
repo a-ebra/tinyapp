e906c40897f5422a16ce80d4aaee911d423e3417
Aqil Ebrahim <aqilebrahim@gmail.com>
2020-12-09 07:38:38 +0000

    added randomt string generator for short URLs

--- express_server.js
+++ express_server.js
@@ -4,6 +4,13 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+//to generate random string 
+const {generateRandomString} = require('./randomString')
+
+//body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -27,10 +34,20 @@ app.get("/hello", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  res.render("urls_index.ejs", templateVars);
+});
+
+//GET route to render erls_new.ejs
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
