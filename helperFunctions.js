66fe1ba3cb33ded97a3f58c496a84b8fe2ea03ec
Aqil Ebrahim <aqilebrahim@gmail.com>
2020-12-13 09:36:19 +0000

    linted helper fxs

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,11 +1,11 @@
-  const urlDatabase = {
-    // "b2xVn2": "http://www.lighthouselabs.ca",
-    // "9sm5xK": "http://www.google.com"
-  };
+const urlDatabase = {
+  // "b2xVn2": "http://www.lighthouselabs.ca",
+  // "9sm5xK": "http://www.google.com"
+};
 
 const generateRandomString = () => {
-  return Math.random().toString(36).substr(2, 6)
-}
+  return Math.random().toString(36).substr(2, 6);
+};
 
 const urlsForUser = (user_id, urlDatabase) => {
   if (urlDatabase[user_id]) {
